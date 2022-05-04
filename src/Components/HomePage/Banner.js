528736fc5c880f40dc4756938f9979f4fe18066a
Tanimismat <tanim.ismat@gmail.com>
2022-05-04 21:12:34 +0600

    banner

--- src/Components/HomePage/Banner.js
+++ src/Components/HomePage/Banner.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    }
     return (
-        <div>
-            <h3>banner</h3>
-        </div>
+        <Carousel fade pause='hover' activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://cdn.motor1.com/images/mgl/MGlA8/s3/long-beach-blue-pearl---acura.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Welcome to AAA AUTO</h3>
+                    <p>Get exclusive offers fast </p>
+                    <p>Volume discount for traders </p>
+                    <button>Become a partner</button>
+                    <button>Log in</button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://www.businessinsider.in/photo/81506863/Master.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Welcome to AAA AUTO</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://images.businessonline.it/news/original/toyota-bz4x-perch-conviene-comprare-o-no-il-suv-elettrico-totalmente-nuovo-pregi-e-difetti-6517041.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Banner from './Banner';
+import Footer from './Footer';
 import Header from './Header';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
-            <h2>this is homepage</h2>
+            <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
