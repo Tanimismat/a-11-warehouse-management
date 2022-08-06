5841163058eee3bd2a25395f5dc8786d019dc0ab
Tanim Ismat <tanim.ismat@gmail.com>
2022-08-06 12:12:02 +0600

    updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,97 +1,111 @@
-import React, { useRef } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
-import auth from '../firebase.init';
-import Loading from './Loading';
-import SocialLogin from './SocialLogin';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import {
+	useSendPasswordResetEmail,
+	useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import auth from "../firebase.init";
+import Loading from "./Loading";
+import SocialLogin from "./SocialLogin";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-    const [
-        signInWithEmailAndPassword,
-        user,
-        loading,
-        error,
-    ] = useSignInWithEmailAndPassword(auth);
+	const [signInWithEmailAndPassword, user, loading, error] =
+		useSignInWithEmailAndPassword(auth);
 
+	const emailRef = useRef("");
+	const passwordRef = useRef("");
 
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
+	const navigate = useNavigate();
 
-    const navigate = useNavigate();
+	const location = useLocation();
 
-    const location = useLocation();
+	let from = location.state?.from?.pathname || "/";
 
-    let from = location.state?.from?.pathname || "/";
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const email = emailRef.current.value;
+		const password = passwordRef.current.value;
+		// console.log(email, password)
 
-    const handleSubmit = (event) => {
+		signInWithEmailAndPassword(email, password);
+	};
 
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        console.log(email, password)
+	const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
-        signInWithEmailAndPassword(email, password);
-    }
+	const navigateToRegister = () => {
+		navigate("/register");
+	};
 
-    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+	if (user) {
+		navigate(from, { replace: true });
+	}
 
-    const navigateToRegister = () => {
-        navigate('/register')
-    }
+	// if (loading || sending) {
+	//     return <Loading></Loading>
+	// }
 
-    if (user) {
-        navigate(from, { replace: true });
-    }
+	const resetPassword = async () => {
+		const email = emailRef.current.value;
+		await sendPasswordResetEmail(email);
+		toast("Email sent");
+	};
 
-    // if (loading || sending) {
-    //     return <Loading></Loading>
-    // }
+	return (
+		<div className="w-50 mx-auto border shadow rounded p-5 m-5 responsive">
+			<h2 className="text-center mb-4">Log in to Account</h2>
+			<Form onClick={handleSubmit}>
+				<Form.Group className="mb-3" controlId="formBasicEmail">
+					<Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+				</Form.Group>
 
-    const resetPassword = async () => {
-        const email = emailRef.current.value;
-        await sendPasswordResetEmail(email);
-        toast('Email sent');
-    }
+				<Form.Group className="mb-3" controlId="formBasicPassword">
+					<Form.Control
+						ref={passwordRef}
+						type="password"
+						placeholder="Password"
+					/>
+				</Form.Group>
 
-    return (
-        <div className='w-50 mx-auto border shadow rounded p-5 m-5 responsive'>
-            <h2 className='text-center mb-4'>Log in to Account</h2>
-            <Form onClick={handleSubmit}>
+				<Button
+					variant="primary"
+					type="submit"
+					style={{
+						width: "200px",
+						height: "40px",
+						backgroundColor: "var(--blue)",
+					}}
+					className="d-block mx-auto border-0 rounded text-white mb-3 ">
+					Login
+				</Button>
+			</Form>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+			<p className="">
+				New to Automotive ?{" "}
+				<Link
+					to="/register"
+					className="pe-auto text-decoration-none"
+					onClick={navigateToRegister}>
+					Please Register
+				</Link>
+			</p>
 
-                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+			<p>
+				Forget password?{" "}
+				<button
+					className="pe-auto border-0 bg-white text-primary"
+					onClick={resetPassword}>
+					Reset password
+				</button>
+			</p>
 
-                </Form.Group>
+			<SocialLogin></SocialLogin>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
-
-                </Form.Group>
-
-                <Button
-                    variant="primary"
-                    type="submit"
-                    style={{ width: '200px', height: '40px', backgroundColor: 'var(--blue)' }}
-                    className='d-block mx-auto border-0 rounded text-white mb-3 '
-                >Login
-                </Button>
-
-            </Form>
-
-            <p className=''>New to Automotive ? <Link to='/register' className='pe-auto text-decoration-none' onClick={navigateToRegister}>Please Register</Link></p>
-
-            <p>Forget password? <button className='pe-auto border-0 bg-white text-primary' onClick={resetPassword}>Reset password</button></p>
-
-            <SocialLogin></SocialLogin>
-
-            <ToastContainer></ToastContainer>
-        </div>
-    );
+			<ToastContainer></ToastContainer>
+		</div>
+	);
 };
 
 export default Login;
